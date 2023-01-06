897f0bc23a77b21064f878b596d018950f77bd23
KushagraVarshney05 <varkushqwerty@gmail.com>
2023-01-06 23:00:11 +0530

    javascript start

---
+++ thapa.js
@@ -0,0 +1,52 @@
+// var myname = "kush";
+// console.log(myname);
+// var whoiam= true;
+
+// console.log(typeof (myname));
+// console.log(typeof (whoiam));
+// a=10;
+// b='20';
+// c=a+b+whoiam;
+// console.log(c);
+// console.log(typeof(c));
+// d=whoiam-a;
+// e=a-b;
+// f=myname-a;
+// console.log(d,e,f);
+// console.log(typeof(f));
+
+
+
+
+// difference between null and undefined
+// var iamuseless=null;
+// console.log(iamuseless);
+// console.log(typeof iamuseless);
+// var iamstandby;
+// console.log(iamstandby);
+// console.log(typeof iamstandby);
+
+
+
+//  what is nan?
+//  nan is a property of the global object.
+//  in other words, it is a variaable in global scope.
+//  the initilal value of nan is Not-A-Number
+
+// var myphonenumber=9876541320;
+// var myname="kushagra";
+// console.log(isNaN(myphonenumber));
+// console.log(isNaN(myname));
+
+// if(isNaN(myname))
+// {
+//   console.log("pls enter a valid number");
+
+// }
+//  let a=10;
+//  let b='10';
+//  console.log(b=a)
+//  console.log(a==b)
+//  console.log(a===b)
+
+
